3c55c8737ad318bdafdba8280949c52652d09bf5
itacamba <daniela.sahagun03@gmail.com>
2020-07-20 01:05:29 -0400

    mobile-nav styles applied, not working yet

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -3,11 +3,13 @@ import React from 'react';
 const MobileNav = () => {
     return (
         <div id="mobile-nav">
-            <a href="/">About</a>
-            <a href="/">Projects</a>
-            <a href="/">Code Challenges</a>
+            <i class="fa fa-times" aria-hidden="true"></i>
+            <a href="/"><i class="fa fa-square" aria-hidden="true"></i>About</a>
+            <a href="/"><i class="fa fa-square" aria-hidden="true"></i>Skills</a>
+            <a href="/"><i class="fa fa-square" aria-hidden="true"></i>Projects</a>
             {/* <a href="#">Technical Writting</a> */}
-            <a href="/">Contact</a>
+            <a href="/"><i class="fa fa-square" aria-hidden="true"></i>Contact</a>
+            <button className="resume-btn">Resume</button>
         </div>
     );
 };
